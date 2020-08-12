a82b119f5f2569094d7cb17d79416e07e95ae943
Stuart Wong <swong31@gmail.com>
2020-08-11 19:57:56 -0700

    Almost have all 8 images populating to home page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function () {
   var searchInput = $("#searchInput");
   var searchBtn = $("#button-addon2");
-  var imgDiv = $("#image");
+  var imgDiv1 = $("#image1");
+  var imgDiv1 = $("#image1");
+  var imgDiv1 = $("#image1");
+  var imgDiv1 = $("#image1");
+  var imgDiv1 = $("#image1");
+  var imgDiv1 = $("#image1");
+  var imgDiv1 = $("#image1");
+  var imgDiv1 = $("#image1");  
+
+  var createImg = $("<img>");
 
   var lastFMURL = "http://ws.audioscrobbler.com/2.0/?method=";
 
@@ -13,23 +22,87 @@ $(document).ready(function () {
 
   var topArtistsdiv = $("#top-artist");
 
-  var arrOfPromises = [];
-  
   var url = lastFMURL + gettopArtists + apiKey;
-  var url2 = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=" 
+  var url2 = "https://www.theaudiodb.com/api/v1/json/1/search.php?s=";
 
-  arrOfPromises.push($.ajax({ url: url, method: "GET" }));
-  arrOfPromises.push($.ajax({ url: url2 + encodeURIComponent(arrOfPromises[0].responseJSON.artists.artist[0].name), method: "GET" }));
-
-  Promise.all(arrOfPromises).then(function (values) {
-
-   
-
-    console.log(encodeURIComponent(arrOfPromises[0].responseJSON.artists.artist[0].name))
-    console.log(arrOfPromises[1])
-
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[0].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
   });
- 
-  
-
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[1].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
+  });
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[2].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
+  });
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[3].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
+  });
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[4].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
+  });
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[5].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
+  });
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[6].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
+  });
+  $.ajax({ url: url, method: "GET" }).then(function (response) {
+    console.log(response);
+    $.ajax({
+      url: url2 + encodeURIComponent(response.artists.artist[7].name),
+      method: "GET",
+    }).then(function (data) {
+      createImg.attr("src", data.artists[0].strArtistThumb);
+      imgDiv.append(createImg);
+    });
+  });
 });
